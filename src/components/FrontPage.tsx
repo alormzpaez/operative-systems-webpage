9495f912468f32539e17d4149c8cf1d375b65469
Alonso Rmz Paez <alonso.rpaez@gmail.com>
2024-04-16 12:11:41 -0600

    Modified Frontpage

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,15 +1,22 @@
 import uanl from "./../assets/uanl.png";
 import fime from "./../assets/fime.png";
 import { Image } from "@nextui-org/react";
+import natividad from "./../assets/natividad.png";
+import emiliano from "./../assets/emiliano.png";
+import alonso from "./../assets/alonso.png";
+import rocio from "./../assets/rocio.png";
+import javier from "./../assets/javier.png";
+import carlos from "./../assets/carlos.png";
+import daniel from "./../assets/daniel.png";
 
 const FrontPage = () => {
   return (
     <div className="dark:text-gray-200">
       <div className="flex flex-col px-56">
         <section className="">
-          <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <div className="max-w-screen-xl px-4 pb-8 mx-auto text-center lg:px-6">
             <div className="max-w-screen-sm mx-auto mb-8 lg:mb-16">
-              <div className="flex p-10 mb-4 text-4xl font-extrabold tracking-tight text-gray-900 border rounded-md justify-evenly dark:text-white">
+              <div className="flex p-10 mb-4 text-4xl font-extrabold tracking-tight text-gray-900 rounded-md justify-evenly dark:text-white">
                 <Image
                   radius="none"
                   width={300}
@@ -45,14 +52,108 @@ const FrontPage = () => {
               <div className="text-center text-gray-500 dark:text-gray-400">
                 <img
                   className="mx-auto mb-4 rounded-full w-36 h-36"
-                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                  src={natividad}
                   alt="Bonnie Avatar"
                 />
                 <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   <a href="#">Natividad Aron De León Ramírez</a>
                 </h3>
-                <p>1855134</p>
+                <div className="flex justify-center gap-5">
+                  <span>1855134</span>
+                  <span>IAS</span>
+                </div>
               </div>
+              <div className="text-center text-gray-500 dark:text-gray-400">
+                <img
+                  className="mx-auto mb-4 rounded-full w-36 h-36"
+                  src={emiliano}
+                  alt="Bonnie Avatar"
+                />
+                <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="#">Emiliano García Montemayor</a>
+                </h3>
+                <div className="flex justify-center gap-5">
+                  <span>2003905</span>
+                  <span>ITS</span>
+                </div>
+              </div>
+              <div className="text-center text-gray-500 dark:text-gray-400">
+                <img
+                  className="mx-auto mb-4 rounded-full w-36 h-36"
+                  src={alonso}
+                  alt="Bonnie Avatar"
+                />
+                <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="#">Alonso Ramírez Páez</a>
+                </h3>
+                <div className="flex justify-center gap-5">
+                  <span>2127873</span>
+                  <span>ITS</span>
+                </div>
+              </div>
+              <div className="text-center text-gray-500 dark:text-gray-400">
+                <img
+                  className="mx-auto mb-4 rounded-full w-36 h-36"
+                  src={rocio}
+                  alt="Bonnie Avatar"
+                />
+                <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="#">Roció Guadalupe Sánchez Medrano</a>
+                </h3>
+                <div className="flex justify-center gap-5">
+                  <span>1959446</span>
+                  <span>IAS</span>
+                </div>
+              </div>
+              <div className="flex justify-center col-start-1 col-end-5 gap-10">
+              <div className="w-1/4 text-center text-gray-500 dark:text-gray-400">
+                <img
+                  className="mx-auto mb-4 rounded-full w-36 h-36"
+                  src={javier}
+                  alt="Bonnie Avatar"
+                />
+                <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="#">Javier López Pérez</a>
+                </h3>
+                <div className="flex justify-center gap-5">
+                  <span>2127884</span>
+                  <span>ITS</span>
+                </div>
+              </div>
+              <div className="w-1/4 text-center text-gray-500 dark:text-gray-400">
+                <img
+                  className="mx-auto mb-4 rounded-full w-36 h-36"
+                  src={carlos}
+                  alt="Bonnie Avatar"
+                />
+                <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="#">Carlos Gabriel Beas Gonzalez</a>
+                </h3>
+                <div className="flex justify-center gap-5">
+                  <span>1940892</span>
+                  <span>ITS</span>
+                </div>
+              </div>
+              <div className="w-1/4 text-center text-gray-500 dark:text-gray-400">
+                <img
+                  className="mx-auto mb-4 rounded-full w-36 h-36"
+                  src={daniel}
+                  alt="Bonnie Avatar"
+                />
+                <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="#">Daniel Aharon Sánchez González</a>
+                </h3>
+                <div className="flex justify-center gap-5">
+                  <span>1967943</span>
+                  <span>ITS</span>
+                </div>
+              </div>
+              </div>
+            </div>
+            <div className="max-w-screen-sm mx-auto mt-16">
+              <p className="text-gray-400">
+                Ciudad Universitaria, San Nicolás de los Garza, Nuevo León a 16 de marzo del 2024
+              </p>
             </div>
           </div>
         </section>
