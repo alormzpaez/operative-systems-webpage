5fdddf5cd307f03818177f17c4e57442e28a11a5
Alonso Rmz Paez <alonso.rpaez@gmail.com>
2024-05-09 18:45:34 -0600

    Modified and finished concurrence

--- src/components/Concurrence.tsx
+++ src/components/Concurrence.tsx
@@ -3,6 +3,7 @@ import { FaCode } from "react-icons/fa";
 import concurrence from "./../assets/concurrence.png";
 import dispatch from "./../assets/dispatch.png";
 import swift from "./../assets/swift.png";
+import cpu from "./../assets/cpu.jpg";
 
 const Concurrence = () => {
   return (
@@ -11,11 +12,11 @@ const Concurrence = () => {
         <section className="">
           <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div className="mr-auto place-self-center lg:col-span-7">
-              <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+              <h1 className="max-w-2xl mb-4 text-6xl font-extrabold leading-none tracking-tight text-white">
                 ¿Cómo funciona la concurrencia en macOS?
               </h1>
-              <p className="max-w-2xl mb-6 mr-10 text-gray-200 lg:mb-8 md:text-lg lg:text-xl">
-                macOS utiliza un sistema de hilos y planificación de procesos
+              <p className="max-w-2xl mb-6 mr-10 text-xl text-justify text-white lg:mb-8">
+                MacOS utiliza un sistema de hilos y planificación de procesos
                 para gestionar la ejecución de múltiples tareas de manera
                 simultánea. Esto se realiza de forma transparente para las
                 aplicaciones, sin requerir que los desarrolladores se preocupen
@@ -28,21 +29,36 @@ const Concurrence = () => {
             </div>
           </div>
         </section>
+        <section className="">
+          <div className="items-center max-w-screen-xl gap-8 px-4 py-8 mx-auto xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+            <img className="w-full" src={cpu} alt="dashboard image" />
+            <div className="mt-4 md:mt-0">
+              <p className="mb-6 text-xl text-justify text-white">
+                El kernel de MacOS gestiona la concurrencia mediante la asignación de tiempo de CPU a cada proceso y la coordinación de los recursos del sistema entre los procesos en ejecución. Utiliza algoritmos de planificación para determinar qué proceso debe ejecutarse en cada momento, priorizando tareas según su importancia y otros criterios definidos por el usuario o el sistema.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="">
+          <p className="mb-6 text-xl text-justify text-white">
+            MacOS también proporciona herramientas y APIs para desarrolladores que les permiten crear aplicaciones concurrentes de manera eficiente y segura. Esto incluye tecnologías como Grand Central Dispatch (GCD) y operaciones asincrónicas, que facilitan la ejecución de tareas en paralelo y la gestión de la concurrencia en aplicaciones MacOS.
+          </p>
+        </section>
         <div className="grid items-center max-w-screen-xl px-4 py-8 pt-0 pb-0 mx-auto lg:gap-8 xl:gap-0 lg:pt-16 lg:grid-cols-12 justify-items-center">
           <div className="col-span-4">
             <Image src={dispatch} alt="Laptop" className="m-0" />
           </div>
           <div className="col-span-8 col-start-5 ml-10">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-5xl">
+            <h1 className="max-w-2xl mb-4 text-5xl font-extrabold leading-none tracking-tight text-white ">
               Grand Central Dispatch
             </h1>
-            <p className="max-w-2xl mb-6 lg:mb-8 md:text-lg lg:text-xl">
+            <p className="max-w-2xl mb-6 text-xl text-justify text-white lg:mb-8">
               Grand Central Dispatch (GCD), también conocido como "libdispatch",
               está disponible tanto en macOS como en iOS. Es una tecnología
               desarrollada por Apple para optimizar el soporte de aplicaciones
               en la ejecución concurrente (multihilo) en hardware multinúcleo.
             </p>
-            <p className="max-w-2xl mb-6 lg:mb-8 md:text-lg lg:text-xl">
+            <p className="max-w-2xl mb-6 text-xl text-justify text-white lg:mb-8">
               GCD proporciona y gestiona colas FIFO a las que tu aplicación
               puede enviar tareas en forma de objetos de bloque. Los bloques
               enviados a las colas de despacho son ejecutados en un grupo de
@@ -55,8 +71,8 @@ const Concurrence = () => {
         </div>
         <section className="">
           <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-16 lg:px-6">
-            <div className="mb-8 lg:mb-16">
-              <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-5xl">
+            <div className="flex justify-center mb-8 lg:mb-16">
+              <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-white">
                 Ejemplos de Uso de GCD en Objetive-C
               </h1>
             </div>
@@ -65,10 +81,10 @@ const Concurrence = () => {
                 <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                   <FaCode className="text-2xl text-blue-600" />
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">
+                <h3 className="mb-2 text-xl font-bold text-white">
                   dispatch_async
                 </h3>
-                <p className="text-gray-500 dark:text-gray-400">
+                <p className="text-xl text-justify text-white">
                   Envía un bloque para su ejecución asíncrona en una cola de
                   despacho y retorna inmediatamente.
                 </p>
@@ -77,10 +93,10 @@ const Concurrence = () => {
                 <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                   <FaCode className="text-2xl text-blue-600" />
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">
+                <h3 className="mb-2 text-xl font-bold text-white">
                   dispatch_once
                 </h3>
-                <p className="text-gray-500 dark:text-gray-400">
+                <p className="text-xl text-justify text-white">
                   Ejecuta un objeto de bloque solo una vez durante la vida útil
                   de una aplicación.
                 </p>
@@ -89,10 +105,10 @@ const Concurrence = () => {
                 <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                   <FaCode className="text-2xl text-blue-600" />
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">
+                <h3 className="mb-2 text-xl font-bold text-white">
                   dispatch_sync
                 </h3>
-                <p className="text-gray-500 dark:text-gray-400">
+                <p className="text-xl text-justify text-white">
                   Envía un objeto de bloque para su ejecución y retorna después
                   de que ese bloque termine de ejecutarse.
                 </p>
@@ -101,10 +117,10 @@ const Concurrence = () => {
                 <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                   <FaCode className="text-2xl text-blue-600" />
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">
+                <h3 className="mb-2 text-xl font-bold text-white">
                   dispatch_async_and_wait
                 </h3>
-                <p className="text-gray-500 dark:text-gray-400">
+                <p className="text-xl text-justify text-white">
                   Envía un elemento de trabajo para su ejecución y retorna solo
                   después de que termine de ejecutarse. A diferencia de
                   dispatch_sync, esta función respeta todos los atributos de la
@@ -115,7 +131,12 @@ const Concurrence = () => {
           </div>
         </section>
         <section className="">
-          <div className="max-w-screen-xl px-4 mx-auto sm:py-16 lg:px-6">
+          <p className="mb-6 text-xl text-justify text-white">
+            Como dato extra, además de proporcionar una amplia gama de herramientas y bibliotecas para desarrolladores experimentados, macOS ofrece una solución específica para aquellos que desean trabajar con el lenguaje de programación Swift.
+          </p>
+        </section>
+        <section className="">
+          <div className="max-w-screen-xl pt-5 pb-16 mx-auto">
             <div className="">
               <div className="flex gap-5">
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-5xl">
